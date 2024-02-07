a5c46dd9c3a65ef109d04ee7f6bc6ec038237867
Katennn <159270155+Katennn@users.noreply.github.com>
2024-02-07 16:41:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const dog = document.getElementById("dog")
+const fire = document.getElementById("fire")
+
+document.addEventListener("keydown", function(event){
+    jump();
+});
+
+function jump () {
+    if (dog.classList !="jump") {
+        dog.classList.add("jump")
+    }
+    setTimeout( function()  {
+        dog.classList.remove("jump")
+    }, 300)
+}
+
+let isAlive = setInterval ( function() {
+    let dogTop = parseInt(window.getComputedStyle(dog).getPropertyValue("top"));
+    let fireLeft = parseInt(window.getComputedStyle(fire).getPropertyValue("left"));
+
+    if (fireLeft < 175 && fireLeft > -50 && dogTop >= 200) {
+        alert("KIRILL, I STILL LOVE YOU DESPITE THE FACT THAT YOU LOST IN THIS GAME!                  P.S. YOUR KATE♡")
+        location.reload()
+    }
+}, 50)
